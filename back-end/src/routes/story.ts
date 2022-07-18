9609ec55e1b500c958ff6b655dca335a5c97449c
Dardaul Hoque <bsse1126@iit.du.ac.bd>
2022-07-18 19:59:24 +0600

    Story backend implemented

--- back-end/src/routes/story.ts
+++ back-end/src/routes/story.ts
@@ -4,6 +4,7 @@ import { removeOldest } from "../utils/removeOldest";
 import { Story } from "../models/Story";
 import { minioClient } from "../server";
 import multer from "multer";
+import { arrayBuffer } from "stream/consumers";
 
 export const story = Router();
 const multerUpload = multer();
@@ -14,7 +15,6 @@ story.post(
   async (req: SecureRequest, res: Response) => {
     try {
       const createdDoc = await Story.create({});
-      // console.log(String(createdDoc._id));
 
       const docSize = await Story.estimatedDocumentCount();
       // if (docSize > 10)
@@ -37,6 +37,24 @@ story.get("/", async (req: SecureRequest, res: Response) => {
 
     res.status(200).json(srcArray);
   } catch (error) {
+    res.status(500).json({
+      errorMessage: "Internal Server Error",
+    });
+  }
+});
+
+story.get("/:src", async (req: SecureRequest, res: Response) => {
+  console.log(req.params.src);
+  const objName = "test1";
+
+  try {
+    const imgBlob = await minioClient.getObject("story", objName);
+    const buffer = await arrayBuffer(imgBlob);
+    const array = new Uint8Array(buffer);
+    res.write(array);
+    res.status(200).send();
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
       errorMessage: "Internal Server Error",
     });
